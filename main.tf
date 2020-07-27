ff58cfb164e75c2d43570d14e1b53403df596fb0
applesoftsol <63569208+applesoftsol@users.noreply.github.com>
2020-07-27 07:26:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,9 +13,8 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  count                = length(var.subnet_names)
   name                 = var.subnet_names[count.index]
   resource_group_name  = data.azurerm_resource_group.network.name
-  address_prefixes     = [var.subnet_prefixes[count.index]]
+  address_prefix     = [var.subnet_prefixes[count.index]]
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
